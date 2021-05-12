c4de4937d4454d6c1f7e9272931bc6d0fe0bd015
Subashininj <83811974+subashininj6586@users.noreply.github.com>
2021-05-12 17:14:24 +0530

    Add files via upload
    1.9: unicafe step4

--- App.js
+++ App.js
@@ -1,41 +1,17 @@
-/*import logo from './logo.svg';
-import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
-*/
 
 import React, { useState } from 'react'
 
-const Condition = ({good,neutral,bad,total,average,positive})=>
-{
-  if(total===0){
-  return(
-  <div>
-<h2> Statistics</h2>
-</div>)
-}
-return(
-<div>
-<Statistics
+const Condition = ({ good, neutral, bad, total, average, positive }) => {
+  if (total === 0) {
+    return (
+      <div>
+        <h2> Statistics</h2>
+      </div>)
+  }
+  return (
+    <div>
+      <Statistics
         good={good}
         neutral={neutral}
         bad={bad}
@@ -44,12 +20,12 @@ return(
         positive={positive}
       />
 
-</div>)
+    </div>)
 }
-const Statistics = ({good,neutral,bad,total,average,positive}) => {
- return(
-<div>
-<h2> Statistics</h2>
+const Statistics = ({ good, neutral, bad, total, average, positive }) => {
+  return (
+    <div>
+      <h2> Statistics</h2>
 
       <p>Good = {good}</p>
       <p>Bad = {bad}</p>
@@ -57,31 +33,31 @@ const Statistics = ({good,neutral,bad,total,average,positive}) => {
       <p>All = {total}</p>
       <p>Average = {average}</p>
       <p>Positive={positive}</p>
-</div>
- )
+    </div>
+  )
 }
 
 
 const App = () => {
-  
+
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
 
-  const total = good+bad+neutral
-  const average=(good*1+bad*-1+neutral*0)/total
-  const positive=(good/total *100 )+"%"
-  
+  const total = good + bad + neutral
+  const average = (good * 1 + bad * -1 + neutral * 0) / total
+  const positive = (good / total * 100) + "%"
+
   const handleGoodClick = () => {
-    
+
     setGood(good + 1)
   }
   const handleBadClick = () => {
-    
+
     setBad(bad + 1)
   }
   const handleNeutralClick = () => {
-    
+
     setNeutral(neutral + 1)
   }
   return (
@@ -91,10 +67,10 @@ const App = () => {
       <button onClick={handleGoodClick}>Good</button>
       <button onClick={handleNeutralClick}>Neutral</button>
       <button onClick={handleBadClick}>Bad</button>
-     
-      <Condition good={good} neutral={neutral} bad={bad} total={total} average={average} positive={positive}/>
-     
-      
+
+      <Condition good={good} neutral={neutral} bad={bad} total={total} average={average} positive={positive} />
+
+
     </div>
   )
 }
